b8316696d76594f024eedd35a9607f57e3c15894
Monika kumari <65735010+monika-4oop@users.noreply.github.com>
2022-10-12 16:49:59 +0530

    final commit

---
+++ src/components/myComp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => {
+  return <div><img src="https://media4.giphy.com/media/hL9q5k9dk9l0wGd4e0/200w.webp?cid=ecf05e47plws5kowvvyb8lrdm58pjwy1edia8rfotb18rlfx&rid=200w.webp&ct=g" alt=""/> </div>;
+};
